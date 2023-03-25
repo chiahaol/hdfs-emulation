10189f43d670e80ce00f3c48a0f17a1f4167145d
Chia-Hao Liu <chliu.official@gmail.com>
2023-03-24 18:40:05 -0700

    put handle_client in a class, so all clients can access shared variables

--- edfs_namenode.py
+++ edfs_namenode.py
@@ -2,19 +2,24 @@ import asyncio
 
 from config import *
 
-async def handle_client(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
+class NameNode:
+    def __init__(self):
+        self.count = 0
 
-    print(f'Client message: {message}')
+    async def handle_client(self, reader, writer):
+        data = await reader.read(100)
+        message = data.decode()
+        self.count += 1
+        print(f'Client message: {message} ({self.count})')
 
-    writer.write(b'NameNode successfully received the message!')
-    await writer.drain()
-    writer.close()
+        writer.write(b'NameNode successfully received the message!')
+        await writer.drain()
+        writer.close()
 
 async def main():
+    namenode = NameNode()
     server = await asyncio.start_server(
-        handle_client, LOCAL_HOST, NAMENODE_PORT)
+        namenode.handle_client, LOCAL_HOST, NAMENODE_PORT)
 
     async with server:
         await server.serve_forever()
